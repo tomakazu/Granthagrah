59b891716796fea19da5f0a1ba5de847caa15e31
Rishabh Malhotra <40312361+tomakazu@users.noreply.github.com>
2021-04-27 11:24:11 +0530

    Create newarrival.php

---
+++ newarrival.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>*student*</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+  <h1>Granthagarah</h1>
+  <br><br><br>
+<h2><a href="library.html"><span style="color:#F17327;margin-left : 1400px;text-transform : none;">Logout</span></a></h2>
+</div>
+<div id="topnav">
+  <ul id="topnavlist">
+    <li><a href="student.html">Home</a></li>
+    <li><a class="active" href="#">New Arrivals</a></li>
+    <li><a href="upcoming.html">Upcoming</a></li>
+  </ul>
+</div>
+<div id="gutter"></div>
+<div id="col1">
+  <h2>Menu</h2>
+  <div id="navcontainer">
+    <ul id="navlist">
+      <li><a href="continue.html">Continue reading</a></li></br>
+      <li><a href="wishlist.html">Wishlist</a></li></br>
+      <li><a href="#">History</a></li>
+    </ul>
+  </div>
+</div>
+<div id="col2">
+  <h2><span style="color:#F17327;">New Arrivals</span></h2>
+<img class="myimage" src="img/b9.jpg"/>
+<img class="myimage" src="img/b10.jpg"/>
+<img class="myimage" src="img/b11.jpg"/>
+<img class="myimage" src="img/b12.png"/>
+<img class="myimage" src="img/b4.jpg"/>
+<img class="myimage" src="img/b3.jpg"/>
+<img class="myimage" src="img/b2.jpg"/>
+<img class="myimage" src="img/b1.jpg"/>
+  </div>
+<div id="col3">
+  <h2>Resources</h2>
+   <div class="searchBox">
+
+            <input class="searchInput"type="text" name="" placeholder="Search">
+            <button class="searchButton" href="#">
+                <i class="material-icons">
+                    search
+                </i>
+            </button>
+        </div>
+</div>
+</body>
+</html>
